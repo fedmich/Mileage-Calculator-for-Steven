15884f5b35ac8ca21e407d67bb031a547937865c
Fedmich <fedmich@gmail.com>
2013-09-29 08:14:59 +0800

    Remove google chart

--- calc.js
+++ calc.js
@@ -129,35 +129,3 @@ function addCommas(nStr)
     return x1 + x2;
 }
 
-
-
-function drawVisualization() {
-  var data = google.visualization.arrayToDataTable([
-    ['x', 'Car', 'Personal Car Costs', 'Rental Car Costs'],
-    ['A',   1,       1,           0.5],
-    ['B',   2,       0.5,         1],
-    ['C',   4,       1,           0.5],
-    ['D',   8,       0.5,         1],
-    ['E',   7,       1,           0.5],
-    ['F',   7,       0.5,         1],
-    ['G',   8,       1,           0.5],
-    ['H',   4,       0.5,         1],
-    ['I',   2,       1,           0.5],
-    ['J',   3.5,     0.5,         1],
-    ['K',   3,       1,           0.5],
-    ['L',   3.5,     0.5,         1],
-    ['M',   1,       1,           0.5],
-    ['N',   1,       0.5,         1]
-  ]);
-
-  new google.visualization.LineChart(document.getElementById('visualization')).
-      draw(data, {curveType: "function",
-		  width: 600, height: 400,
-		  vAxis: {maxValue: 10}
-		  , 'chartArea': {'width': '100%', 'height': '80%'}
-		  , 'legend': {'position': 'bottom'}
-		  }
-  );
-}
-google.load('visualization', '1', {packages: ['corechart']});
-google.setOnLoadCallback(drawVisualization);
